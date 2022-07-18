b20b127038c2d95f618329d253ecd3398b5a4824
mauer4 <mauer4@gmail.com>
2022-07-18 18:11:51 +0300

    This code works perfectly

--- CLOCK.sv
+++ CLOCK.sv
@@ -1,110 +1,59 @@
 `timescale 1 ps / 1 ps
-
-// add counter logic_range
-// check why time resets when going from SET to CLK
-
-module sec_inc_clk(input logic [5:0] sec_in, input logic [5:0] min_in, input logic [4:0] hr_in,
-               input logic set_AMPM,
-               output logic [5:0] sec_out, output logic [5:0] min_out, output logic [4:0] hr_out);
-   always_comb begin
-       min_out = min_in;
-       hr_out = hr_in;
-       if(sec_in < 6'd59)
-         sec_out = sec_in + 1'b1;
-       else begin
-         sec_out = 0;
-         if(min_in < 6'd59)
-           min_out = min_in + 1'b1;
-         else begin
-           min_out = 0;
-           if(hr_in < 4'd23)
-             hr_out = hr_in + 1'b1;
-           else
-             hr_out = 0;
-         end
-       end
-   end
-
-endmodule: sec_inc_clk
+`define Fifty_M 26'b10111110101111000010000000
 
 
 module CLOCK(input logic clk, input logic rst,
              input logic set_min, input logic set_hr, input logic AM2PM,
              output logic [5:0] sec, output logic [5:0] min, output logic [4:0] hr);
 
-    enum {WAIT, CLK_SET, CLK_RUN} state, next_state;
+    enum bit {CLK_RUN, CLK_SET} TMOD;
 
     logic [25:0] counter, next_counter;
+    logic rst_counter;
 
     // register for the time
     logic [5:0] next_sec, next_min;
     logic [4:0] next_hr;
-
-    // registers for the set mechanism
-    logic [5:0] add_min, next_add_min;
-    logic [4:0] add_hr, next_add_hr;
-
-    //input and output of the increase time module
-    logic [5:0] sec_increased_out, min_increased_out;
-    logic [4:0] hr_increased_out;
-
-    sec_inc_clk increase_second(sec, min, hr, set_AMPM, sec_increased_out, min_increased_out, hr_increased_out);
+    logic rst_sec, rst_min, rst_hr;
+    logic inc_sec, inc_min, inc_hr;
 
     //state transition logic
     always_ff @(negedge rst)
-      state <= next_state;
-
-    always_comb begin
-      next_state = WAIT;
-      case(state)
-        WAIT: next_state = CLK_SET;
-        CLK_SET: next_state = CLK_RUN;
-        CLK_RUN: next_state = CLK_SET;
-      endcase
-    end
+      TMOD <= (TMOD == CLK_RUN) ? CLK_SET : CLK_RUN ;
 
     //clk time registers
     always_ff @(posedge clk) begin
-      if(~rst && state == WAIT) {sec, min, hr, counter} <= 43'b0;
-      else if(counter == 26'b0) {sec, min, hr} <= {next_sec, next_min, next_hr};
-
-      counter <= next_counter;
-      if(counter == 26'b10111110101111000010000000)
+      counter <= counter + 1'b1;
+      if(counter == `Fifty_M)
         counter <= 0;
     end
 
-    always_comb begin
-      {next_sec, next_min, next_hr} = {sec, min, hr};
-      case(state)
-        WAIT: {next_sec, next_min, next_hr, next_counter} = 43'b0;
-        CLK_SET: {next_sec, next_min, next_hr, next_counter} = {7'b0, next_add_min, next_add_hr, 26'b0};
-        CLK_RUN: {next_sec, next_min, next_hr, next_counter} = {sec_increased_out, min_increased_out, hr_increased_out, counter + 1'b1};
-      endcase
-    end
+    assign rst_counter = (counter == `Fifty_M) ? 1'b1 : 1'b0;
+    assign inc_sec = TMOD ? 1'b0 : rst_counter;
 
-    //SET_CLK register logic
-    always_ff @(posedge set_min, negedge rst) begin
-      //if(~rst) add_min <= min;
-      //else
-      add_min <= next_add_min;
-    end
+    always_ff @(posedge inc_sec)
+      sec <= next_sec;
 
-    always_ff @(posedge set_hr, negedge rst) begin
-      if(~rst) add_hr <= hr;
-      else if(AM2PM) add_hr <= (next_add_hr < 5'd12) ? next_add_hr + 5'd12 - 1'b1 : next_add_hr - 5'd12 - 1'b1;
-      else add_hr <= next_add_hr;
-    end
+    assign rst_sec = (sec == 6'd59) ? 1'b1 : 1'b0;
 
-    always_comb begin
-      {next_add_min, next_add_hr} = {add_min, add_hr};
-      case(state)
-        WAIT: {next_add_min, next_add_hr} = {12'b0};
-        CLK_SET: begin
-          next_add_min = (add_min < 6'd59) ? add_min + 1'b1 : 0;
-          next_add_hr = (add_hr < 5'd23) ? add_hr + 1'b1 : 0;
-        end
-        CLK_RUN: {next_add_min, next_add_hr} = {min, hr};
-      endcase
-    end
+    assign next_sec = rst_sec ? 6'b0 : sec + 1'b1;
+
+    assign inc_min = TMOD ? set_min : rst_sec;
+
+    always_ff @(posedge inc_min)
+      min <= next_min;
+
+    assign rst_min = (min == 6'd59) ? 1'b1 : 1'b0;
+
+    assign next_min = rst_min ? 6'b0 : min + 1'b1;
+
+    assign inc_hr = TMOD ? set_hr : rst_min;
+
+    always_ff @(posedge inc_hr)
+      hr <= next_hr;
+
+    assign rst_hr = (hr == 5'd23) ? 1'b1 : 1'b0;
+
+    assign next_hr = rst_hr ? 5'b0 : hr + 1'b1;
 
 endmodule: CLOCK
